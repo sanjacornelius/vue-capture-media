23aeb34507b7ec2ad9880164cb1c9291043fc3ef
Sanja <sanja.cornelius@processmaker.com>
2024-08-06 10:21:05 -0700

    Update isMobile detection to properly capture iPads

--- src/mixins/MobileDetection.js
+++ src/mixins/MobileDetection.js
@@ -2,8 +2,18 @@ export default {
   computed: {
     isMobile() {
       const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-      
-      return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
-    }
+
+      // Regular expression to match various mobile devices
+      const mobileRegex = /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i;
+
+      // Check for iPad separately
+      const isIpad = /iPad/.test(userAgent) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
+
+      if (mobileRegex.test(userAgent) || isIpad) {
+          return true;
+      } else {
+          return false;
+      }
+    },
   }
 };
